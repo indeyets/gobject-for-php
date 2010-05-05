e33b15effb7c0b2307722c631f31a1c897145055
Alexey Zakhlestin <indeyets@gmail.com>
2010-05-05 16:16:27 +0400

    use pake's plugin for building instead of local tasks

--- Pakefile.php
+++ Pakefile.php
@@ -1,70 +1,30 @@
 <?php
 
-pake_desc('Clean all temporary files');
-pake_task('clean');
+if (version_compare(pakeApp::VERSION, '1.4.1', '<'))
+    throw new pakeException('Pake 1.4.1 or newer is required');
 
-pake_task('configure');
-
-pake_task('build', 'configure');
-
-pake_desc('configure, build and install extension');
-pake_task('install', 'build');
-
-pake_desc('reconfigure (needed if you change config.m4 file)');
-pake_task('reconfigure', 'clean', 'configure');
+pake_import('phpExtension');
 
 pake_desc('display information about extension');
 pake_task('info');
 
 pake_desc('run GDB');
-pake_task('debug');
+pake_task('debug_local_test');
 
 pake_desc('try to execute test.php script');
-pake_task('test');
-
-function run_configure()
-{
-    if (!file_exists('configure'))
-        pake_sh('phpize');
-
-    if (!file_exists('Makefile')) {
-        // pake_sh('CC=/opt/llvm/bin/clang '.realpath('configure'));
-        pake_sh(realpath('configure'));
-    }
-}
-
-function run_reconfigure() {} // virtual task
-
-function run_build()
-{
-    pake_sh('make', true);
-}
-
-function run_install()
-{
-    pake_superuser_sh('make install');
-}
-
-function run_clean()
-{
-    if (file_exists('Makefile'))
-        pake_sh('make distclean');
-
-    if (file_exists('configure'))
-        pake_sh('phpize --clean');
-}
+pake_task('local_test');
 
 function run_info()
 {
     echo pake_sh('php --re gobject');
 }
 
-function run_debug()
+function run_debug_local_test()
 {
-    pake_sh('gdb php', true);
+    pake_sh('gdb --args php test.php', true);
 }
 
-function run_test()
+function run_local_test()
 {
     echo pake_sh("php test.php");
 }
