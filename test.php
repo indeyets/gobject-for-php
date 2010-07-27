55780754270c7c2df53139a9201181a730991f46
Alexey Zakhlestin <indeyets@gmail.com>
2010-07-27 21:46:50 +0400

    test for more complicated signal-cases

--- test.php
+++ test.php
@@ -1,20 +1,25 @@
 <?php
-$obj = new Glib\GObject\GObject;
-$hdl = $obj->connect('php_test', function($param){
+$obj = new Glib\GObject\Type;
+$obj->name = 'test';
+$obj->parent = 'GObject';
+
+$s1 = new Glib\GObject\Signal(0, array('Glib\GObject\GObject'));
+$s2 = new Glib\Gobject\Signal();
+
+$obj->signals['something1'] = $s1;
+$obj->signals['something2'] = $s2;
+$obj->generate();
+
+$obj2 = new test;
+$hdl = $obj2->connect('something1', function($obj, $param){
+    echo "Hello ".$param.' (I got '.get_class($obj).")\n";
+}, 'test');
+
+$hdl = $obj2->connect('something2', function($param){
     echo "Hello ".$param."\n";
 }, 'test');
 
-$obj->emit('php_test');
-$obj->emit('php_test');
+$tmp = new Glib\GObject\GObject;
 
-$obj->disconnect($hdl);
-echo "disconnected\n";
-
-unset($obj);
-
-echo "bye!\n";
-
-// $obj->emit('php_test');
-// $obj->emit('php_test');
-// 
-// $obj->emit('php_test2');
+$obj2->emit('something1', $tmp);
+$obj2->emit('something2');
