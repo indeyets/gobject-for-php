05d6a3a8a2396029b744936cb40781c8c54fc662
Alexey Zakhlestin <indeyets@gmail.com>
2010-09-07 12:22:49 +0400

    removed irrelevant parts from test.php

--- test.php
+++ test.php
@@ -4,14 +4,11 @@ use Glib\GObject\ParamSpec as GParamSpec;
 $type = new Glib\GObject\Type;
 $type->name = 'test';
 $type->parent = 'GObject';
-// $type->signals['something1'] = $s1;
-// $type->signals['something2'] = $s2;
 $type->properties[] = GParamSpec::string('field1', GParamSpec::READABLE|GParamSpec::WRITABLE);
 $type->properties[] = GParamSpec::boolean('field2', GParamSpec::READABLE|GParamSpec::WRITABLE);
 $type->properties[] = GParamSpec::long('field3', GParamSpec::READABLE|GParamSpec::WRITABLE);
 $type->generate();
 
-// $tmp = new Glib\GObject\GObject;
 
 $obj = new test;
 echo "1:\n";
@@ -29,9 +26,3 @@ echo "2:\n";
 var_dump($obj->field2);
 echo "1:\n";
 var_dump($obj->field1);
-
-// echo "=-=-=-=-=-=-=-=-=-\n";
-// echo " Emitting\n";
-// echo "=-=-=-=-=-=-=-=-=-\n";
-// $obj->emit('something1', $tmp);
-// $obj2->emit('something2');
